e13292b4bb34b301e28010ba1dbed7984d9d73fd
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-03-25 23:00:43 +0000

    Random Card Generator casi terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let valor = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let pinta = ["♦", "♥", "♠", "♣"];
+
+  function cardRandom(array) {
+    colorPinta();
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function colorPinta() {
+    if (printPinta == "♦" || printPinta == "♥") {
+      document.getElementById("pinta-superior").style.color = "red";
+      document.getElementById("pinta-inferior").style.color = "red";
+    }
+  }
+
+  let printPinta = `${cardRandom(pinta)}`;
+  document.getElementById("pinta-superior").innerHTML = printPinta;
+  document.getElementById("pinta-inferior").innerHTML = printPinta;
+
+  let printValor = `${cardRandom(valor)}`;
+  document.getElementById("valor").innerHTML = printValor;
 };
